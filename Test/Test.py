807b99e65d1abbf30f4b6fae0a30aae2cd0741c8
CMPL3X <gustavsandersons@gmail.com>
2023-10-27 23:06:02 +0300

    Update

---
+++ Test/Test.py
@@ -0,0 +1,57 @@
+#pip install opencv-python-headless tensorflow pyautogui
+
+from keras.models import load_model
+import cv2
+import numpy as np
+
+# Load the model
+model = load_model("keras_model.h5", compile=False)
+
+# Load the labels
+class_names = open("labels.txt", "r").readlines()
+
+# CAMERA can be 0 or 1 based on the default camera of your computer
+camera = cv2.VideoCapture(0)
+
+while True:
+    # Grab the web camera's image.
+    ret, image = camera.read()
+
+    # Resize the raw image into (224-height,224-width) pixels
+    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+
+    # Show the image in a window
+    cv2.imshow("Webcam Image", image)
+
+    # Make the image a numpy array and reshape it to the model's input shape.
+    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
+
+    # Normalize the image array
+    image = (image / 127.5) - 1
+
+    # Predict the model
+    prediction = model.predict(image)
+    index = np.argmax(prediction)
+    confidence_score = prediction[0][index]
+
+    # Print prediction and confidence score
+    if index == 0:
+        print("Class: Normal", end="")
+    elif index == 1:
+        print("Class: Left", end="")
+    elif index == 2:
+        print("Class: Right", end="")
+    elif index == 3:
+        print("Class: Rotate", end="")
+
+    print(" Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+
+    # Listen to the keyboard for presses.
+    keyboard_input = cv2.waitKey(1)
+
+    # 27 is the ASCII for the esc key on your keyboard.
+    if keyboard_input == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
