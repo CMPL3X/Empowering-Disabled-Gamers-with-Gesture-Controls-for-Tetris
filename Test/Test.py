41226b2565f7e48b20d4157cb71b1833ca4548ca
CMPL3X <gustavsandersons@gmail.com>
2023-10-28 13:39:38 +0300

    Update

--- Test/Test.py
+++ Test/Test.py
@@ -1,57 +1,67 @@
-#pip install opencv-python-headless tensorflow pyautogui
+# This code is used to test, if the model is working corectly.
+# Just put both (the .h5 and .txt files in the same folder!)
+# pip install teachable-machine
+# pip install opencv-python
 
-from keras.models import load_model
+from teachable_machine import TeachableMachine
 import cv2
 import numpy as np
+import io
+from PIL import Image
 
-# Load the model
-model = load_model("keras_model.h5", compile=False)
+camera_port = 2 # REPLACE this with your accual camera port. You can check with the cameraPortTest.py code
 
-# Load the labels
-class_names = open("labels.txt", "r").readlines()
+# Replace these paths with the absolute paths to your model and labels files
+model_path = "C:\\Users\\54132\\OneDrive\\Dators\\CoralTest\\Git\\Empowering-Disabled-Gamers-with-Gesture-Controls-for-Tetris\\Test\\keras_model.h5"
+labels_path = "C:\\Users\\54132\\OneDrive\\Dators\\CoralTest\\Git\\Empowering-Disabled-Gamers-with-Gesture-Controls-for-Tetris\\Test\\labels.txt"
 
-# CAMERA can be 0 or 1 based on the default camera of your computer
-camera = cv2.VideoCapture(0)
+model = TeachableMachine(model_path=model_path, labels_file_path=labels_path)
+
+# Open the camera stream
+cap = cv2.VideoCapture(camera_port)
 
 while True:
-    # Grab the web camera's image.
-    ret, image = camera.read()
+    _, img = cap.read()
 
-    # Resize the raw image into (224-height,224-width) pixels
-    image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
+    # Convert the image (numpy array) to bytes
+    img_bytes = cv2.imencode('.jpg', img)[1].tobytes()
+    
+    # Classify the image
+    result = model.classify_image(io.BytesIO(img_bytes))
 
-    # Show the image in a window
-    cv2.imshow("Webcam Image", image)
-
-    # Make the image a numpy array and reshape it to the model's input shape.
-    image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-
-    # Normalize the image array
-    image = (image / 127.5) - 1
-
-    # Predict the model
-    prediction = model.predict(image)
-    index = np.argmax(prediction)
-    confidence_score = prediction[0][index]
+    # Extract classification results
+    class_index = result["class_index"]
+    class_name = result["class_name"]
+    class_confidence = result["class_confidence"]
+    predictions = result["predictions"]
 
     # Print prediction and confidence score
-    if index == 0:
-        print("Class: Normal", end="")
-    elif index == 1:
-        print("Class: Left", end="")
-    elif index == 2:
-        print("Class: Right", end="")
-    elif index == 3:
-        print("Class: Rotate", end="")
+    print("Class Index:", class_index)
+    print("Class Name:", class_name)
+    print("Class Confidence:", class_confidence)
+    print("Predictions:", predictions)
 
-    print(" Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
+    # Show the image in a window
+    cv2.imshow("Webcam Image", img)
 
-    # Listen to the keyboard for presses.
+    # Check if the class name is one of your predefined classes (0, 1, 2, 3)
+    if class_name in ["0", "1", "2", "3"]:
+        # Handle the specific class
+        if class_name == "0":
+            print("Detected class 0")
+        elif class_name == "1":
+            print("Detected class 1")
+        elif class_name == "2":
+            print("Detected class 2")
+        elif class_name == "3":
+            print("Detected class 3")
+
+    # Listen to the keyboard for presses
     keyboard_input = cv2.waitKey(1)
 
-    # 27 is the ASCII for the esc key on your keyboard.
+    # 27 is the ASCII for the Esc key on your keyboard
     if keyboard_input == 27:
         break
 
-camera.release()
+cap.release()
 cv2.destroyAllWindows()
